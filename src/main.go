39e40a7646ee2a0c3547afce90a16c0f0a5de94c
msk-1 <h69.masaki@gmail.com>
2021-11-18 01:29:37 +0900

    add sample

--- src/main.go
+++ src/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 
+	sample "github.com/go-get/sample"
 	common "github.com/msk-1/go-get-common/common"
 )
 
 func main() {
+	sample.Sample()
 	common.Common()
 	fmt.Print("hello")
 }
